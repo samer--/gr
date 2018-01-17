05c113f09d546686077593787eae94c74f8beebb
samer-- <samer.abdallah.00@gmail.com>
2018-01-17 00:49:24 +0000

    Remove debugging informational

--- lib/gks/quartz/GKSView.m
+++ lib/gks/quartz/GKSView.m
@@ -966,7 +966,7 @@ static void seg_xform_rel(double *x, double *y) { }
       double width  = (viewport[1] - viewport[0]) * ppm_x;
       double height = (viewport[3] - viewport[2]) * ppm_y;
 
-      NSLog(@"current size: %lf x %lf, target size: %lf x %lf", p->width, p->height, width, height);
+      /* NSLog(@"current size: %lf x %lf, target size: %lf x %lf", p->width, p->height, width, height); */
       if (fabs(p->width - width) > 1e-9 || fabs(p->height - height) > 1e-9)
         {
           NSRect rect = [[self window] frame];
